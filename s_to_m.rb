c5c4f2367cdd06242c9e4386b30075cd963b9fef
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-31 19:17:49 -0400

    Correc the to_minutes method and added the calling

--- s_to_m.rb
+++ s_to_m.rb
@@ -3,9 +3,14 @@ def to_minutes(arr)
     n = arr.count
     new_arr = []
     n.times do |i|
-        minutes = (arr[i]/60).to_i
+        minutes = (arr[i]/60)
         new_arr.push(minutes)
     end
     return new_arr
 end
 
+#Calling the method
+seconds = [100, 50, 1000, 5000, 1000, 500]
+minutes = to_minutes(seconds)
+
+print "#{minutes}\n" 
